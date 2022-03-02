907928f145667c5101eaaf71f0ffb361877a42c5
randhysatrio <randhysatrio@gmail.com>
2022-03-02 11:05:39 +0700

    Update MySQL configuration

--- database/index.js
+++ database/index.js
@@ -1,16 +1,17 @@
 const mysql = require('mysql');
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'lewishamilton',
-  database: 'mysql_express_practice',
+  host: '85.10.205.173',
+  user: 'randhy_satrio',
+  password: 'randhy86satrio',
+  database: 'mysql_express_vl',
+  port: 3306,
   multipleStatements: true,
 });
 
 db.connect((err, result) => {
   if (err) {
-    console.log(err);
+    return console.log(err);
   }
   console.log('Connected to MySQL');
 });
